be75d51f08d5be55eed086dcf83d9759489ff3bf
erichislop <eric.l.hislop@gmail.com>
2018-09-12 19:44:01 +1200

    WIP task 2.1

--- routes/bookings.js
+++ routes/bookings.js
@@ -32,16 +32,45 @@ router.get("/:startdate&:enddate", function(req, res, next) {
 function isBetweenDates(startDate, endDate) {
   console.log(startDate);
   console.log(endDate);
-  console.log('2018-03-12T00:00:00.000')
+  sDate = moment(startDate);
+  eDate = moment(endDate);
+  // console.log('2018-03-12T00:00:00.000')
 
-  const range = moment.range(startDate, endDate);
+  // const range = moment.range(startDate, endDate);
 
-  var bookingdate = moment('2018-03-12T00:00:00.000');
-  bookingdate.within(range);
-  console.log(bookingdate.within(range));
-  console.log(range);
+  // var bookingdate = moment('2018-03-12T00:00:00.000');
+  // bookingdate.within(range);
+  // console.log(bookingdate.within(range));
+  // console.log(range);
+  var bookingStartDate = moment(bookings[0].startDate);
+  var bookingEndDate = moment(bookings[0].endDate);
+
+  var isBetween = bookingStartDate >= sDate && bookingEndDate <= eDate;
+  console.log(bookingStartDate);
+  console.log(bookingEndDate);
+  console.log(eDate);
+  console.log(sDate);
+  console.log(isBetween);
+
 }
 
+// Illustration:
+//
+// startdate                          enddate
+// v                                        v
+// #----------------------------------------#
+//
+//         #----------------------#
+//         ^                      ^
+//         startD              endD
+
+// return startD >= startdate && endD <= enddate;
+// {
+//   "id": "dsku28",
+//   "propertyId": "iua7869",
+//   "startDate": "2018-03-10T12:00:00.000Z",
+//   "endDate": "2018-03-18T12:00:00.000Z"
+// },
 
 //  - 2.1. All bookings for a **given period** (start and end dates).
  
