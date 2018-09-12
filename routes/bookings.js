d07bc6cbb864ecc2e0860bedbe1f55e2f7e98902
erichislop <eric.l.hislop@gmail.com>
2018-09-12 17:54:56 +1200

    WIP task 2.1

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var fs = require('fs');
+
+var 
+   Moment = require("moment"),
+   MomentRange = require("moment-range"), 
+   moment = MomentRange.extendMoment(Moment);
 var router = express.Router();
 
 var bookings;
@@ -15,8 +20,43 @@ router.get('/', function(req, res, next) {
   res.json(bookings);
 });
 
-router.get('/new', function(req, res, next) {
-    console.log(bookings);
-    res.json(bookings);
-})
+router.get("/:startdate&:enddate", function(req, res, next) {
+  var bookingsBetweenRange = isBetweenDates(req.params.startdate, req.params.enddate)
+  res.json(bookingsBetweenRange);
+});
+
+// "startDate": "2018-03-10T12:00:00.000Z",
+// "endDate": "2018-03-18T12:00:00.000Z"
+
+
+function isBetweenDates(startDate, endDate) {
+  console.log(startDate);
+  console.log(endDate);
+  console.log('2018-03-12T00:00:00.000')
+
+  const range = moment.range(startDate, endDate);
+
+  var bookingdate = moment('2018-03-12T00:00:00.000');
+  bookingdate.within(range);
+  console.log(bookingdate.within(range));
+  console.log(range);
+}
+
+
+//  - 2.1. All bookings for a **given period** (start and end dates).
+ 
+//     2.1.a - The bookings returned should have the startDate and endDate formated to dd/MM/YYYY HH:mm 
+//     using the property timeZone. 
+    
+//     2.1.b - The **given period** is also provided in the Property timeZone.
+
+// - 2.2. All bookings longer or equal to 25 days.
+
+// - 2.3. All bookings shorter or equal to 3 days.
+
+// - 2.4. All Properties with Bookings with 1 day or less between bookings
+
+// - 2.5. With the functions created expose them in a restful application and create a dashboard (simple UI) to display the results
+
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,37 +1,47 @@
 var express = require("express");
 var fs = require("fs");
 var router = express.Router();
-var NodeGeocoder = require("node-geocoder");
 
-var users;
-fs.readFile("./store/users.json", "utf8", function (err, data) {
-  if (err) throw err;
-  users = JSON.parse(data);
-});
+var usersFile = fs.readFileSync("./store/users.json");
+users = JSON.parse(usersFile);
 
-var geocoder = require("local-reverse-geocoder");
+var propertiesFile = fs.readFileSync("./store/properties.json");
+properties = JSON.parse(propertiesFile);
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
+router.get("/", function(req, res, next) {
   res.json(users);
 });
 
 // 1.1. All users from a particular city
 
+router.get("/properties", function(req, res, next) {
+  res.json(properties);
+});
+
+// - 1.3. All users from Free tier subscription which have more than 6 properties
+
+router.get("/properties6", function(req, res, next) {
+  var sixProperties = freeTier6Properties();
+  res.json(sixProperties);
+});
+
+// - 1.4. All users from Premium tier subscription which have less than 4 properties
+
+router.get("/properties4", function(req, res, next) {
+  var fourProperties = premiumTier4Properties();
+  res.json(fourProperties);
+});
 
 // 1.2. All users from a particular company (a user of a company is a user that has the same email domain.
 // E.g for john@getproperly.com and company@getproperly.com "getproperly.com" is the email domain)
 
-router.get("/:emaildomain", function (req, res, next) {
+router.get("/:emaildomain", function(req, res, next) {
   var emailD = searchEmail(req.params.emaildomain);
   res.json(emailD);
 });
 
 function searchEmail(emailDomain) {
-  // for each email
-  // get string after @
-  // if it matchs provided string
-  // return it
   var usersEmail = [];
   for (i = 0; i < users.length; i++) {
     if (users[i].email.includes(emailDomain)) {
@@ -41,61 +51,48 @@ function searchEmail(emailDomain) {
   return usersEmail;
 }
 
-
-// - 1.3. All users from Free tier subscription which have more than 6 properties
-
-router.get("/properties6", function (req, res, next) {
-  var sixProperties = freeTier6Properties();
-  res.json(sixProperties);
-});
-
-//
-var properties;
-fs.readFile("./store/properties.json", "utf8", function (err, data) {
-  if (err) throw err;
-  properties = JSON.parse(data);
-});
-
 function freeTier6Properties() {
-  var properties6 = []
+  var properties6 = [];
+  var usersWith6 = [];
   for (i = 0; i < users.length; i++) {
-    if (users[i].subscriptionId === "sub01" || users[i].subscriptionId === "sub02") {
+    if (
+      users[i].subscriptionId === "sub01" ||
+      users[i].subscriptionId === "sub02"
+    ) {
       for (j = 0; j < properties.length; j++) {
-        if (properties[i].userID === users[i].id) {
+        if (properties[j].userId === users[i].id) {
           properties6.push(properties[i]);
+          if (properties6.length > 6) {
+            usersWith6.push(users[i]);
+            properties6 = [];
+          }
         }
       }
     }
   }
-  if (properties6.length > 6) {
-    return properties6;
-  }
+  return usersWith6;
 }
 
-// [
-//   {
-//       "id": "iua7869",
-//       "userId": "kua876",
-//       "title": "Nomads Apartment - 1",
-//       "location": [
-//           -36.845919,
-//           174.767400
-//       ],
-//       "type": "Apartment",
-//       "numberOfRooms": 3,
-//       "timeZone": "Pacific/Auckland"
-//   },
-
-// [
-//   {
-//       "id": "kua876",
-//       "name": "John Snow",
-//       "email": "john@acme.com",
-//       "location": [
-//           -36.847610,
-//           174.767379
-//       ],
-//       "subscriptionId": "sub01"
-//   },
+function premiumTier4Properties() {
+  var properties4 = [];
+  var usersWith4 = [];
+  for (i = 0; i < users.length; i++) {
+    if (
+      users[i].subscriptionId === "sub03" ||
+      users[i].subscriptionId === "sub04"
+    ) {
+      for (j = 0; j < properties.length; j++) {
+        if (properties[j].userId === users[i].id) {
+          properties4.push(properties[i]);
+        }
+      }
+      if (properties4.length < 4) {
+        usersWith4.push(users[i]);
+        properties4 = [];
+      }
+    }
+  }
+  return usersWith4;
+}
 
 module.exports = router;
