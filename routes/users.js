65579a1ac0663670f69644c26e00ab639e23d2fd
erichislop <eric.l.hislop@gmail.com>
2018-09-14 14:38:57 +1200

    WIP task 1.1

--- routes/users.js
+++ routes/users.js
@@ -9,27 +9,42 @@ var propertiesFile = fs.readFileSync("./store/properties.json");
 properties = JSON.parse(propertiesFile);
 
 /* GET users listing. */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   res.json(users);
-});
-
-router.get("/properties", function(req, res, next) {
-  res.json(properties);
 });
 
 // 1.1. All users from a particular city
 //TODO
 
+// router.get("/usersfromsamecity", function (req, res, next) {
+//   geocoder.lookUp(point, function (err, res) {
+//   });
+//   res.json(properties);
+// });
+
+
+// var geocoder = require('local-reverse-geocoder');
+
+// geocoder.init({}, function () {
+//   // geocoder is loaded and ready to run
+// });
+// var point = { latitude: -33.940275, longitude: 25.565720 };
+// geocoder.lookUp(point, function (err, res) {
+
+// });
+
+
+
 // - 1.3. All users from Free tier subscription which have more than 6 properties
 
-router.get("/properties6", function(req, res, next) {
+router.get("/properties6", function (req, res, next) {
   var sixProperties = freeTier6Properties();
   res.json(sixProperties);
 });
 
 // - 1.4. All users from Premium tier subscription which have less than 4 properties
 
-router.get("/properties4", function(req, res, next) {
+router.get("/properties4", function (req, res, next) {
   var fourProperties = premiumTier4Properties();
   res.json(fourProperties);
 });
@@ -38,7 +53,7 @@ router.get("/properties4", function(req, res, next) {
 // E.g for john@getproperly.com and company@getproperly.com "getproperly.com" is the email domain)
 
 /*example http://localhost:3000/users/@acme.com */
-router.get("/:emaildomain", function(req, res, next) {
+router.get("/:emaildomain", function (req, res, next) {
   var emailD = searchEmail(req.params.emaildomain);
   res.json(emailD);
 });
